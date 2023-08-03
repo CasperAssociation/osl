130fb7f30ced7ed1e6fc96831f91acf8cad8767a
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2023-08-03 12:34:23 -0400

    amend sum test cases

--- test/OSL/Spec/SumSpec.hs
+++ test/OSL/Spec/SumSpec.hs
@@ -15,6 +15,7 @@ import OSL.SimplifyType (complexifyValueUnsafe, simplifyType)
 import OSL.TranslatedEvaluation (evalTranslatedFormula3, evalTranslatedFormula4, evalTranslatedFormula6, evalTranslatedFormula7, evalTranslatedFormula8, evalTranslatedFormula9, evalTranslatedFormula10)
 import OSL.Types.Argument (Argument (Argument), Statement (Statement), Witness (Witness))
 import OSL.Types.ArgumentForm (ArgumentForm (ArgumentForm), StatementType (StatementType), WitnessType (WitnessType))
+import OSL.Types.ErrorMessage (ErrorMessage (ErrorMessage))
 import OSL.Types.FileName (FileName (FileName))
 import OSL.Types.OSL (ContextType (Global), Declaration (Defined), Name (Sym), Type (N, Fin, Product), ValidContext)
 import OSL.Types.Value (Value (Nat, Pair', Fin'))
@@ -78,35 +79,63 @@ exampleSpec c = do
       evalTranslatedFormula3 c "sumIs" argumentForm (exampleArgument c)
         `shouldBe` Right True
 
+    it "a negative case" $
+      evalTranslatedFormula3 c "sumIs" argumentForm (exampleUnsoundArgument c)
+        `shouldBe` Right False 
+
   describe "sum spec's semantics are preserved in codegen stage 4" $ do
     it "a positive case" $
       evalTranslatedFormula4 c "sumIs" argumentForm (exampleArgument c)
         `shouldBe` Right True
+
+    it "a negative case" $
+      evalTranslatedFormula4 c "sumIs" argumentForm (exampleUnsoundArgument c)
+        `shouldBe` Right False 
 
   describe "sum spec's semantics are preserved in codegen stage 6" $ do
     it "a positive case" $
       evalTranslatedFormula6 (1 :: RowCount) c "sumIs" argumentForm (exampleArgument c)
         `shouldBe` Right ()
 
+    it "a negative case" $
+      evalTranslatedFormula6 (1 :: RowCount) c "sumIs" argumentForm (exampleUnsoundArgument c)
+        `shouldBe` Left (ErrorMessage Nothing "evaluate: \"quantifierFreeFormulaIsTrue\": not satisfied on the following rows: [(0,Just False)]")
+
   describe "sum spec's semantics are preserved in codegen stage 7" $ do
     it "a positive case" $
       evalTranslatedFormula7 (1 :: RowCount) (8 :: BitsPerByte) c "sumIs" argumentForm (exampleArgument c)
         `shouldBe` Right ()
+
+    it "a negative case" $
+      evalTranslatedFormula7 (1 :: RowCount) (8 :: BitsPerByte) c "sumIs" argumentForm (exampleUnsoundArgument c)
+        `shouldBe` Left (ErrorMessage Nothing "evalTrace: polynomial constraint not satisfied: (\"assert\",1 + 28948022309329048855892746252171976963363056481941560715954676764349967630336x29,0^1,0,SubexpressionTrace {value = 0, stepType = 21, adviceValues = fromList [(32,0),(33,0),(34,0)]},27,fromList [(5,0),(6,0),(7,0),(8,0),(9,0),(10,0),(11,0),(12,0),(13,0),(14,0),(15,0),(16,0),(17,0),(18,0),(19,0),(20,0),(21,0),(22,0),(23,0),(24,0),(25,0),(26,0),(27,1),(28,0),(29,0),(30,0),(31,0),(32,0),(33,0),(34,0)])")
 
   describe "sum spec's semantics are preserved in codegen stage 8" $ do
     it "a positive case" $
       evalTranslatedFormula8 (1 :: RowCount) (8 :: BitsPerByte) c "sumIs" argumentForm (exampleArgument c)
         `shouldBe` Right ()
 
+    it "a negative case" $
+      evalTranslatedFormula8 (1 :: RowCount) (8 :: BitsPerByte) c "sumIs" argumentForm (exampleUnsoundArgument c)
+        `shouldBe` Left (ErrorMessage Nothing "evaluate: \"assert\": not satisfied on the following rows: [(19,Just 1)] out of 256")
+
   describe "sum spec's semantics are preserved in codegen stage 9" $ do
     it "a positive case" $
       evalTranslatedFormula9 (1 :: RowCount) (8 :: BitsPerByte) c "sumIs" argumentForm (exampleArgument c)
         `shouldBe` Right ()
 
+    it "a negative case" $
+      evalTranslatedFormula9 (1 :: RowCount) (8 :: BitsPerByte) c "sumIs" argumentForm (exampleUnsoundArgument c)
+        `shouldBe` Left (ErrorMessage Nothing "evaluate: \"assert\": not satisfied on the following rows: [(19,Just 1)] out of 257")
+
   describe "sum spec's semantics are preserved in codegen stage 10" $ do
     it "a positive case" $
       evalTranslatedFormula10 (1 :: RowCount) (8 :: BitsPerByte) c "sumIs" argumentForm (exampleArgument c)
         `shouldBe` Right ()
+
+    it "a negative case" $
+      evalTranslatedFormula10 (1 :: RowCount) (8 :: BitsPerByte) c "sumIs" argumentForm (exampleUnsoundArgument c)
+        `shouldBe` Left (ErrorMessage Nothing "evaluate: \"assert\": not satisfied on the following rows: [(19,Just 1)] out of 257")
 
 complexStatementType :: Type ()
 complexStatementType =
